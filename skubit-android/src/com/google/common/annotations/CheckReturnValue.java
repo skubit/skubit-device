4907175fc4038db66da8e8405718d68efcd3e5ea
Shane Isbell <shane.isbell@gmail.com>
2014-10-25 02:19:46 -0700

    Remove guava library but add specific classes needed

---
+++ skubit-android/src/com/google/common/annotations/CheckReturnValue.java
@@ -0,0 +1,12 @@
+package com.google.common.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckReturnValue {
+
+}
